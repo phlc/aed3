ddafa936df1c86ea6e14b6d0d979e71a17a744a1
phlc <pedrocarvalho@outlook.com>
2020-08-28 18:43:01 -0300

    create

--- crud/crud.java
+++ crud/crud.java
@@ -2,10 +2,31 @@
 Classe CRUD - Realiza Operacoes CRUD de Arquivo
 */
 class crud{
-   //atributos
+//atributos da classe
+   private static int id = 0;
+   private String file;
+
+//atributos
    byte lapide;
    short tam;
    byte[] objeto;
+
+//construtor
+   crud(String file){
+      this.
+   }
+
+//metodos
+   /*
+   create - Cria um registro no arquivo
+   @param T objeto
+   @return int in
+   */
+   public int create(T objeto){
+
+   }
    
+   /*
+   read - Ler um objeto do arquivo
 
 }
