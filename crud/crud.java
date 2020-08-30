2741067528899ee3abe395bedb24d4e7c60e9c33
phlc <pedrocarvalho@outlook.com>
2020-08-29 21:31:50 -0300

    constructor + layouts

--- crud/crud.java
+++ crud/crud.java
@@ -1,26 +1,66 @@
 /*
 Classe CRUD - Realiza Operacoes CRUD de Arquivo
+Cabecalho
+   byte 0-5 tipo do arquivo "CRUD"
+   byte 6-9 proximo id (int) inicio em 1
+Registros
+   lapide - 1 byte
+   indicador de tamanho - 1 short
+   conteudo = byte[]
+
 */
-class crud{
+
+import java.lang.reflect.Constructor;
+import java.io.RandomAccessFile;
+
+class crud <T extends Registro>{
 //atributos da classe
-   private static int id = 0;
-   private String file;
 
 //atributos
-   byte lapide;
-   short tam;
-   byte[] objeto;
+   private Constructor<T> constructor; 
+   private String file;
 
 //construtor
-   crud(String file){
-      this.
+   crud(Constructor<T> constructor, String file){
+      this.constructor = constructor;
+      this.file = file;
+      RandomAccessFile arq;      
+
+      //Verificar se arquivo com nome ja existe
+      try{
+         arq = new RandomAccessFile (file, "r");
+      }
+      //Se nao exisitr criar
+      catch (FileNotFoundException novoArq){
+         arq = new RandomAccessFile (file, "rw");
+         
+         //Escreve CRUD no cabecalho para identificar o tipo do arquivo
+         arq.writeUTF("CRUD");
+         
+         //Escreve o proximo id do arquivo (offset 6)
+         arq.writeInt(1);
+
+         //voltar cursor para inicio
+         arq.seek(0);
+      }
+      //testa se arquivo do tipo crud
+      try{
+         String nome = arq.readUTF();
+         if (!nome.equals("CRUD")
+            throws new Exception("Arquivo !CRUD");
+      }
+      catch (Exception e){
+         System.out.println(e);
+         arq.close();
+         this.file = null;
+      }
    }
 
 //metodos
    /*
    create - Cria um registro no arquivo
    @param T objeto
-   @return int in
+   @return int id
    */
    public int create(T objeto){
 
@@ -28,5 +68,27 @@ class crud{
    
    /*
    read - Ler um objeto do arquivo
+   @param int id
+   @return T objeto
+   */
+   public T read(int id){
+
+   }
+   
+   /*
+   update - atualiza um registro
+   @param T objeto
+   @return boolean true (sucesso) false (falha)
+   */
+   public boolean update(T objeto){
+
+   }
+
+   /*
+   delete - deleta um registro
+   @param int id
+   @return boolean true (sucesso) false (falha)
+   */
+
 
 }
